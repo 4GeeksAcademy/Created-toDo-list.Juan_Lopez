2cb2803275126bf54e4ebfbe5905b37a0f7a3933
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2024-01-18 18:40:51 +0000

    step1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,25 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleClick = () => {
+    console.log("se ejecuta onclick");
+  };
+
+  const handleChange = () => {
+    console.log("escribiendo");
+    console.log(event.targ);
+  };
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">ToDo list</h1>
+
+      <div>
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleClick}>Set up ToDo</button>
+      </div>
+    </div>
+  );
 };
+
 
 export default Home;
