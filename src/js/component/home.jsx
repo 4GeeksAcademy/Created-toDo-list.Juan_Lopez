e6e4b8c2bbdee4750d796c728f62fb50615a4070
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2024-01-18 20:01:05 +0000

    step 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [newToDo, setNewToDo] = useState("tarea agregada");
+  const [todos, setToDos] = useState("tarea 1", "tarea 2", "tarea 3");
+
   const handleClick = () => {
     console.log("se ejecuta onclick");
   };
 
   const handleChange = () => {
-    console.log("escribiendo");
-    console.log(event.targ);
+    console.log(event.target.value);
+    setNewToDo(event.target.value);
   };
   return (
     <div className="text-center">
@@ -21,9 +24,20 @@ const Home = () => {
         <input type="text" onChange={handleChange} />
         <button onClick={handleClick}>Set up ToDo</button>
       </div>
+      <p>
+        Nueva tarea agregada: <b>{newToDo}</b>
+      </p>
+      <ul>
+        {todos.map((todo) =>{
+          return(
+            <li>
+              {todo}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 };
-
 
 export default Home;
